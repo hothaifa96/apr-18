a05fc084ece89505e077eaf097bc010fc3074b93
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-04-18 18:41:11 +0300

    my new commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(' tagiiidi aba shelaaakh ganan ');
+
+var input = 'yes'
+
+if (input){
+    console.log('yeeeaaaah')
+}
+else{
+    console.log('leh leh habibi')
+}
---
+++ main.py
@@ -0,0 +1,2 @@
+print(f'i am the man call me at {input("enter tyour phone number")}')
+
